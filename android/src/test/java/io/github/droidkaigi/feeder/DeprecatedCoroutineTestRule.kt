5483ed8d00a2174ffbaa5e686897cacc283fcbd3
takahirom <takam.dev@gmail.com>
2022-03-12 17:16:58 +0900

    Fix tests

--- android/src/test/java/io/github/droidkaigi/feeder/DeprecatedCoroutineTestRule.kt
+++ android/src/test/java/io/github/droidkaigi/feeder/DeprecatedCoroutineTestRule.kt
@@ -2,25 +2,28 @@ package io.github.droidkaigi.feeder
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
+@Deprecated(
+    replaceWith = ReplaceWith("io.github.droidkaigi.feeder.CoroutineTestRule"),
+    message = "Use CoroutineTestRule. For using Kotlin 1.6 test dispatcher"
+)
 @ExperimentalCoroutinesApi
-class CoroutineTestRule : TestWatcher() {
-    private val dispatcher = StandardTestDispatcher()
-    val scope = TestScope(dispatcher)
+class DeprecatedCoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
+    TestWatcher() {
 
     override fun starting(description: Description?) {
         super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description?) {
         super.finished(description)
         Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
     }
 }
