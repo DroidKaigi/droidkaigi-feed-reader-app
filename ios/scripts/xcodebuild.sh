aa186f33af05afb48098c42ddea116d3d5500fdd
ryoya ito <ry.itto.github@gmail.com>
2022-04-29 23:12:16 +0900

    Specify project

--- ios/scripts/xcodebuild.sh
+++ ios/scripts/xcodebuild.sh
@@ -6,7 +6,7 @@ CONFIGURATION=${3}
 
 SWIFT_RUN="swift run -c release --package-path Tools"
 WORKSPACE=DroidKaigiFeeder.xcworkspace
-PLATFORM_IOS="iOS Simulator,name=iPhone 12 Pro,OS=15.0"
+PLATFORM_IOS="iOS Simulator,name=iPhone 12 Pro,OS=15.4"
 
 echo "⚙️  Building $SCHEME..."
 set -o pipefail && xcodebuild $TYPE \
